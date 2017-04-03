624263c640dc106f96b4f33cd697b605d0b597dc
Rofiqi86 <elzarficky@gmail.com>
2017-04-03 15:08:01 +0700

    warning level 11

---
+++ src/tugas8_jawaban/Tugas8_jawaban.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugas8_jawaban;
+
+/**
+ *
+ * @author Prasetya Nina
+ */
+public class Tugas8_jawaban {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         Form1 it=new Form1();
+        it.setVisible(true);
+    }
+}
